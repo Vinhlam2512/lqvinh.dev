4d3d31b551aa900b4e2da3be465b30707072b6d9
vinhlq <vinhlq2512@gmail.com>
2025-05-12 06:29:41 +0700

    add layout my-cv

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {},
+      spacing: {
+        72: '18rem' // Add custom spacing value
+      },
+      fontSize: {
+        xxl: '2.5rem' // Add custom font size
+      }
+    }
+  },
+  plugins: []
+};
